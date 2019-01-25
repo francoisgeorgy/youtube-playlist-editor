ece94bbe24ebc52b8c6a3ac7cbb94cbaa7197aeb
François Georgy <francois.georgy@gmail.com>
2019-01-25 13:49:58 +0100

    Updates

---
+++ precache-manifest.fc5ebf9130e366beee0d2bd6bdc6303d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e47717370390b6478782",
+    "url": "/static/js/main.e4771737.chunk.js"
+  },
+  {
+    "revision": "ef9a8ad3082580222564",
+    "url": "/static/js/1.ef9a8ad3.chunk.js"
+  },
+  {
+    "revision": "e47717370390b6478782",
+    "url": "/static/css/main.7aaec2ad.chunk.css"
+  },
+  {
+    "revision": "43a5f7634aa2f568f55dfc973b6b6550",
+    "url": "/index.html"
+  }
+];
