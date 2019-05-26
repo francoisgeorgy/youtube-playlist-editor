4f38d40ea03c4b5f4e60db81906e055009db4ed5
François Georgy <francois.georgy@gmail.com>
2019-05-26 12:14:16 +0200

    Updates

---
+++ precache-manifest.e93e9958f44ab983c3969b4ae45e2f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eaa7781fe871c615f03af256bc0ca94",
+    "url": "/youtube-playlist-editor/index.html"
+  },
+  {
+    "revision": "b6a29de4b88be8b84c54",
+    "url": "/youtube-playlist-editor/static/css/main.891d5a68.chunk.css"
+  },
+  {
+    "revision": "82ee341d354d477494cc",
+    "url": "/youtube-playlist-editor/static/js/2.8b4a0be3.chunk.js"
+  },
+  {
+    "revision": "b6a29de4b88be8b84c54",
+    "url": "/youtube-playlist-editor/static/js/main.366f4010.chunk.js"
+  },
+  {
+    "revision": "209e90200f1af76d3279",
+    "url": "/youtube-playlist-editor/static/js/runtime~main.0ce3f3d4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtube-playlist-editor/precache-manifest.ced3371891e36b0a4bb4710792ee1b9b.js"
+  "/youtube-playlist-editor/precache-manifest.e93e9958f44ab983c3969b4ae45e2f31.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/youtube-playlist-editor/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/youtube-playlist-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
